8e09f6d279a075b678bea43d028b136cfabb403d
hsouth95 <hsouth95@hotmail.co.uk>
2016-10-22 20:54:25 +0100

    Implemented async uploading & fixed issues with upload url

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import webapp2
 from google.appengine.ext import ndb
 from google.appengine.api import users
 from google.appengine.ext import blobstore
+from google.appengine.api import modules
+from google.appengine.api import urlfetch
 from google.appengine.ext.webapp import blobstore_handlers
 
 template_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.getcwd()))
@@ -14,23 +16,43 @@ class MainPage(webapp2.RequestHandler):
         upload_url = blobstore.create_upload_url('/upload')
         template = template_env.get_template('home.html')
         context = {
-            'upload_url': upload_url
+            'upload_url': upload_url,
+            'title': "WS Ltd Prototype"
         }
         self.response.out.write(template.render(context))
 
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    def get(self):
+        upload_url = blobstore.create_upload_url('/upload')
+        self.response.out.write(upload_url)
+
+        # If the user goes to this URL, redirect them to the main page
+        self.redirect('/')
+
     def post(self):
         try:
             upload = self.get_uploads()[0]
             name = self.request.get("filename")
-            photo = UserPhoto(name=name, blob_key=upload.key())
+            photo = File(name=name, blob_key=upload.key())
 
             photo.put()
-        except:
-            self.error(500)
 
-class UserPhoto(ndb.Model):
+            upload_url = blobstore.create_upload_url('/upload')
+
+            self.redirect('/upload')
+        except Exception as e:
+            self.response.out.write(e.strerror);
+
+class ApiHandler(webapp2.RequestHandler):
+    def get(self):
+        module_url = 'http://{}'.format(modules.get_hostname(module='api'))
+
+        result = urlfetch.fetch(module_url)
+        if result.status_code == 200:
+            self.response.out.write(result.content)
+
+class File(ndb.Model):
     name = ndb.StringProperty()
     blob_key = ndb.BlobKeyProperty()
 
-app = webapp2.WSGIApplication([('/', MainPage),('/upload', UploadHandler)], debug=True)
+app = webapp2.WSGIApplication([('/', MainPage),('/upload', UploadHandler), ('/hello', ApiHandler)], debug=True)
--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,28 @@ $(function(){
     var fileSelected = false,
         loadingIcon = "<div class='spinner'><div class='double-bounce1'></div><div class='double-bounce2'></div></div>";
 
+    /**
+     * A function to handle the picking of a file
+     * @param {Object} evt - The file picking event e.g. drag, pick
+     */
     pickFile = function(evt) {
       var target = evt.dataTransfer || evt.target || window.event.srcElement,
           files = target.files;
 
-      $(".drag-n-drop-content").html(loadingIcon);
+      setLoading();
       $("#filename").val(files[0].name.split(".")[0]);
       handleFileType(files[0]);
     }
 
+    /** Indicates to the user that something is loading */
+    setLoading = function(){
+        $(".drag-n-drop-content").html(loadingIcon);
+    }
+
+    /**
+     * Chooses a different handler for each file type e.g. Audio, Image
+     * @param {File} file - The file that is being uploaded
+     */
     handleFileType = function(file){
       var imgRegex = /^image\//,
           audioRegex = /^audio\//,
@@ -31,6 +44,10 @@ $(function(){
       }
     }
 
+    /**
+     * Parses the image and displays to the user
+     * @param {File} image - The image file to be handled
+     */
     handleImage = function(image){
       if(FileReader && image) {
           var fr = new FileReader();
@@ -43,6 +60,10 @@ $(function(){
       }
     }
 
+    /**
+     * Displays the image in the upload box
+     * @param {string} imageUrl - The url of the image to be displayed
+     */
     appendImage = function(imageUrl){
         var uploadedImage = document.createElement("img"),
             image = new Image();
@@ -52,11 +73,16 @@ $(function(){
 
         $(".drag-n-drop-content").html(uploadedImage);
         $(".drag-n-drop-overlay").removeClass("pointer");
+        $("#submit").removeAttr("disabled");
 
         addField("height", "Height", "number", image.height);
         addField("width", "Width", "number", image.width);
     }
 
+    /**
+     * Utility function to cancel the default behaviour of an event
+     * @param {Event} e - The event to cancel
+     */
     cancel = function(e) {
       if (e.preventDefault) {
           e.preventDefault();
@@ -64,6 +90,13 @@ $(function(){
       return false;
     }
 
+    /**
+     * Creates an input field inside the upload form
+     * @param {string} name - The name of the input field
+     * @param {string} labelText - The text in the input field to be displayed
+     * @param {string} type - The type of the input field
+     * @param {integer} value - The value inside the field
+     */
     addField = function(name, labelText, type, value) {
         var inputElement = document.createElement("input"),
             labelElement = document.createElement("label");
@@ -89,17 +122,47 @@ $(function(){
         }
     }
 
+    /**
+     * Resets the form to allow for additional uploads
+     * @param {boolean} success - Indicates if we're resetting because of a successful upload
+     */
     resetForm = function(success) {
         $("input[type=text],input[type=number]").not("#filename").remove();
         $("label[for!='filename']").remove();
         $("#file-form")[0].reset();
+        $("#submit").attr("disabled", "true");
 
         $(".drag-n-drop-overlay").addClass("pointer");
 
         if(success) {
-            $(".drag-n-drop-content").html("<i class='fa fa-upload' aria-hidden='true'></i>&nbsp;Upload a file!");
+            $(".drag-n-drop-content").html("<i class='fa fa-upload' aria-hidden='true'></i>&nbsp;Drag or pick a file to upload.");
         } else {
             $(".drag-n-drop-content").html("Opps! Something went wrong, please try again.");
+        }
+    }
+
+    /** Asynchronously uploads the file and the fields to the server */
+    submitForm = function(){
+        var url = $("#file-form").attr("action");
+
+        // To allow for async file uploads we require FormData, IE 9 does not use this
+        if(typeof FormData !== "undefined"){
+            var formData = new FormData($("#file-form")[0]);
+
+            $.ajax({
+                url: url,
+                type: "POST",
+                data: formData,
+                beforeSend: function(){
+                    setLoading();
+                },
+                processData: false,  // tell jQuery not to process the data
+                contentType: false,
+                success: function(data){
+                    $("#file-form").attr("action", data);
+                    resetForm(true);
+                }
+            })
         }
     }
 
@@ -114,7 +177,7 @@ $(function(){
         e = e || window.event;
         if (e.preventDefault) {
             e.preventDefault();
-         }
+        }
 
          pickFile(e.originalEvent);
          return false;
@@ -122,10 +185,29 @@ $(function(){
 
     $("#submit").on("click", function(e){
         e = e || window.event;
-        if(e.preventDefault){
+
+        // If the FormData object does not exist then we have to synchronously upload the file
+        // therefore we rely on the form action
+        if(e.preventDefault && typeof FormData !== "undefined"){
             e.preventDefault();
+            submitForm();
         }
     });
 
     document.getElementById("file-picker").onchange = pickFile;
+
+    /*
+     * To prevent DDOS attempts, Google has a 10-minute limit on the upload URL
+     * therefore every 9 minutes we request a new URL.
+     * SEE: https://groups.google.com/forum/#!topic/google-appengine/55__MQ61EO0
+     */
+    setInterval(function(){
+        $.ajax({
+            url: "/upload",
+            type: "GET",
+            success: function(data){
+                $("#file-form").attr("action", data);
+            }
+        })
+    }, 1000 * 60 * 9)
 });
