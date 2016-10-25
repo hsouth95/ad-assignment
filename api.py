283010c53e6fe39fdde285b77aa7448d1733aadd
musepig1995 <hsouth95@hotmail.co.uk>
2016-10-25 15:37:13 +0100

    Got the basic endpoint working

--- api.py
+++ api.py
@@ -8,50 +8,22 @@ from google.appengine.ext.blobstore import BlobKey
 
 from endpoints_proto_datastore.ndb import EndpointsModel
 
-class FileModel(ndb.Model):
-    name = ndb.StringProperty(required=True)
+class FileModel(EndpointsModel):
+    _message_fields_schema = ('id', 'name', 'blob_key')
+
+    name = ndb.StringProperty()
     blob_key = ndb.BlobKeyProperty()
 
-class File(messages.Message):
-    key = messages.IntegerField(1, required=True)
-    name = messages.StringField(2)
-    blob_key = messages.StringField(3)
+@endpoints.api(name='fileapi', version='v1', description='API for Files')
+class FileApi(remote.Service):
 
-class FileList(messages.Message):
-    items = messages.MessageField(File, 1, repeated = True)
-
-@endpoints.api(name='fileapi', version='v1', description='API for files')
-class FileService(remote.Service):
-    @endpoints.method(File, File,
-                      name='file.insert',
-                      path='file',
-                      http_method='POST')
-    def insert_file(self, request):
-        file = FileModel(name = request.name, blob_key = BlobKey(request.blob_key))
-
+    @FileModel.method(path='file', http_method='POST', name='file.insert')
+    def FileInsert(self, file):
         file.put()
+        return file
 
-        return request
+    @FileModel.query_method(path='files', name='file.list')
+    def FileList(self, query):
+        return query
 
-    @endpoints.method(message_types.VoidMessage, FileList,
-                      name='file.list',
-                     path='files',
-                      http_method='GET')
-    def list_files(self, request):
-        results = FileModel.query().fetch(10, keys_only=True)
-        if results is None:
-            raise endpoints.NotFoundException('No files found.')
-
-        files = []
-        for item in results:
-            files.append(File(key=item.id()))
-        return FileList(items=files)
-
-#    @endpoints.method(GET_RESOURCE, FILE,
-#                      path='files/{id}',
-#                      http_method='GET',
-#                      name='files.get')
-#    def get_file(self, request):
-#        return File(name="yolo", blob_key="ajghag")
-
-app = endpoints.api_server([FileService], restricted=False)
+app = endpoints.api_server([FileApi], restricted=False)
