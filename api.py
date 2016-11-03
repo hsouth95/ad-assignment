d7410392ffc3323f474e9004873f4feb2be444a4
musepig1995 <hsouth95@hotmail.co.uk>
2016-11-03 12:54:31 +0000

    Reverted back

--- api.py
+++ api.py
@@ -14,22 +14,19 @@ class FileModel(EndpointsModel):
     name = ndb.StringProperty()
     file_type = ndb.StringProperty()
     blob_key = ndb.BlobKeyProperty()
-    owner = ndb.UserProperty()
     created = ndb.DateTimeProperty(auto_now_add=True)
 
-@endpoints.api(name='metaapi', version='v1', description='API for File meta data',
-               audiences=[endpoints.API_EXPLORER_CLIENT_ID])
+@endpoints.api(name='metaapi', version='v1', description='API for File meta data')
 class MetaApi(remote.Service):
     @FileModel.method(user_required=True,
                       path='file', http_method='POST', name='file.insert')
     def FileInsert(self, file):
-        file.owner = endpoints.get_current_user()
         file.put()
         return file
 
     @FileModel.query_method(query_fields=('limit', 'order', 'pageToken'),
-                            path='files', name='file.list', user_required=True)
+                            path='files', name='file.list')
     def FileList(self, query):
-        return query.filter(FileModel.owner == endpoints.get_current_user())
+        return query
 
 app = endpoints.api_server([MetaApi], restricted=False)
