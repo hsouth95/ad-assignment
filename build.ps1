5c9224795f3d084264aea1d600d25777c798c9ac
Harrison South <hsouth95@hotmail.co.uk>
2016-12-09 02:38:15 +0000

    Split out CSS and updated build script to minify and concat said css

--- build.ps1
+++ build.ps1
@@ -1,1 +1,11 @@
-﻿uglifyjs scripts/auth.js scripts/header.js -o scripts/assignment.min.js -m -c
+﻿param(
+    [switch]$install = $false
+)
+
+if ($install) {
+    npm install clean-css -g
+    npm install uglifyjs -g
+}
+
+#uglifyjs scripts/auth.js scripts/header.js -o scripts/assignment.min.js -m -c
+Get-Content styles/header.css, styles/body.css, styles/loading.css, styles/utility.css | cleancss -o styles/main.min.css
